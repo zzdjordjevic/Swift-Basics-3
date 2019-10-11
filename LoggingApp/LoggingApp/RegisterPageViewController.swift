3f349fe875ba9be3d405b61d275b43f0ce64daba
zzdjordjevic <zdjordjeciv@carnegietechnologies.com>
2019-10-11 17:36:17 +0200

    UI, Registration page and first Ui test testTapButton

---
+++ LoggingApp/LoggingApp/RegisterPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterPageViewController.swift
+//  LoggingApp
+//
+//  Created by Zorana Djordjevic on 10/1/19.
+//  Copyright © 2019 Zorana Djordjevic. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LoggingApp/LoggingAppUITests/UITestConstants.swift
@@ -0,0 +1,9 @@
+//
+//  UITestConstants.swift
+//  LoggingAppUITests
+//
+//  Created by Zorana Djordjevic on 10/3/19.
+//  Copyright © 2019 Zorana Djordjevic. All rights reserved.
+//
+
+import Foundation
