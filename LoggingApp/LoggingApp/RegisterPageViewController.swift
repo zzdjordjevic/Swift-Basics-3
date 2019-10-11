dfa6e364261064cf511f7a3e69b8a3c16094c27f
zzdjordjevic <zdjordjeciv@carnegietechnologies.com>
2019-10-11 17:36:50 +0200

    More testing

--- LoggingApp/LoggingApp/RegisterPageViewController.swift
+++ LoggingApp/LoggingApp/RegisterPageViewController.swift
@@ -10,21 +10,67 @@ import UIKit
 
 class RegisterPageViewController: UIViewController {
 
+    @IBOutlet weak var userNameRegistration: UITextField!
+    @IBOutlet weak var passwordRegistration: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
+    @IBOutlet weak var singUpRegistration: UIButton!
+    
+    private func displayAlert(with message: String) {
+        let alert = UIAlertController(title: message, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction.init(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "performLoginSegue" {
+            guard let userNameText = userNameRegistration.text, !userNameText.isEmpty else {
+                displayAlert(with: "You didn't enter your User Name")
+                return false
+            }
+            guard let userPassword = passwordRegistration.text, !userPassword.isEmpty else {
+                displayAlert(with: "You didn't enter Password")
+                return false
+            }
+            return true
+            
+        }
+        return true
+    }
+    
+    //    @IBAction func registerButtonTapped(_ sender: Any) {
+//        
+//        let userName = userNameRegistration.text
+//        let password = passwordRegistration.text
+//        
+//        //Check empty fields
+//        if (userName!.isEmpty || password!.isEmpty) {
+//            //Display message
+//            display(userMessage: "All fields are required")
+//           
+//            return
+//        }
+//
+//        
+//    }
+//    func display(userMessage: String) {
+//        var myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert)
+//        
+//        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//        
+//        myAlert.addAction(okAction)
+//        
+//        self.present(myAlert, animated: true, completion: nil)
+//    }
+    
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
